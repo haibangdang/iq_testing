54f2c36e3909024ba7673c111012abdef0553c9e
haibangdang <haibang.dang@gmail.com>
2023-09-14 01:30:13 +0700

    login test

--- src/configs/auth.ts
+++ src/configs/auth.ts
@@ -1,6 +1,6 @@
 export default {
   meEndpoint: '/auth/me',
-  loginEndpoint: '/jwt/login',
+  loginEndpoint: 'https://iqtest-server.onrender.com/api/users/login',
   registerEndpoint: '/jwt/register',
   storageTokenKeyName: 'accessToken',
   onTokenExpiration: 'refreshToken' // logout | refreshToken
